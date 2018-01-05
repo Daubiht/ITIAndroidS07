4b1a36b81ce2cbc2906a05e20fd3285d00eec757
Daubiht <daubihtmania@gmail.com>
2018-01-05 17:51:46 +0100

    Liste exo :
    "Ajouter un élément"
    "Créer un layout"
    "mutualiser les valeurs"
    "@runtime"
    "définir des actions"

--- ITIAndroidS07/app/src/main/java/com/example/daubiht/itiandroids07/MainActivity.java
+++ ITIAndroidS07/app/src/main/java/com/example/daubiht/itiandroids07/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.daubiht.itiandroids07;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv_hw = (TextView)findViewById(R.id.tv_hello_world);
+        String now = DateUtils.formatDateTime(getApplicationContext(), (new Date()).getTime(), DateFormat.FULL);
+        tv_hw.setText(now);
+    }
+
+    public void Clicked(View v) {
+        Toast.makeText(getApplicationContext(),"test",Toast.LENGTH_LONG).show();
+    }
+
+    public void notification_test() {
+
     }
 }
